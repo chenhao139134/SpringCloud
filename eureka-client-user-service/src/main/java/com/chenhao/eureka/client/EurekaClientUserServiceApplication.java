69348e5ffb5aeddd406e287774effadb8913f128
chenhao139134 <1479189756@qq.com>
2020-07-19 16:47:35 +0800

    eureka

---
+++ eureka-client-user-service/src/main/java/com/chenhao/eureka/client/EurekaClientUserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.chenhao.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientUserServiceApplication.class, args);
+    }
+
+}
